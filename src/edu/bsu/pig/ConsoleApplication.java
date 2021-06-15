d1cc26584516123d4922f9d042cd1d15bb117575
Nirav Varma <nirav.iitk@gmail.com>
2021-06-16 01:20:41 +0200

    Reset turn score to zero upon bust

--- src/edu/bsu/pig/ConsoleApplication.java
+++ src/edu/bsu/pig/ConsoleApplication.java
@@ -9,31 +9,43 @@ import java.util.Scanner;
  */
 public final class ConsoleApplication {
 
-    public static void main(String[] args) {
-        Player player1 = new Player("Nirav");
-        Player player2 = new Player("Kiran");
-        Game game = new Game(player1, player2);
-        Scanner scanner = new Scanner(System.in);
+    private static Player player1 = new Player("Nirav");
+    private static Player player2 = new Player("Kiran");
+    private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        Game game = new Game(player1, player2);
         System.out.println("Welcome to the game of Pig");
         Turn turn = new Turn(player1, new RolledDie());
         while(!game.isOver()) {
-            System.out.println(player1.getName() + "'s score is " + player1.getScore());
-            System.out.println(player2.getName() + "'s score is " + player2.getScore());
-            System.out.println("It is " + game.currentPlayer().getName() + "'s turn");
-            System.out.println("Press enter to roll");
-            String line = scanner.nextLine();
-            turn.roll();
+            playOneTurn(game);
+        }
+        printScores();
+        System.out.println("Game over: Winner is " + game.getWinner().getName());
+    }
+
+    private static void playOneTurn(Game game) {
+        printScores();
+        System.out.println("It is " + game.currentPlayer().getName() + "'s turn");
+        System.out.println("This turn's score is " + game.currentTurn().getScore());
+        System.out.println("Press enter to roll, q to end turn");
+        String line = scanner.nextLine();
+        if(line.startsWith("q")) {
+            game.endTurn();
+        } else {
+            game.roll();
+        }
 
             /* The following is kind of introducing cheat code in the game
             if(line.startsWith("!")) {
                 player1.setScore(100);
             }
              */
-            game.endTurn();
-        }
+    }
 
-        System.out.println("Game over: Winner is " + game.getWinner().getName());
+    private static void printScores() {
+        System.out.println(player1.getName() + "'s score is " + player1.getScore());
+        System.out.println(player2.getName() + "'s score is " + player2.getScore());
     }
 
 }
